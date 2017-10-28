dce7efadfb4a8ea1b7aff55dbc48bcd3b71a60ba
Edgar Marca <matiskay@gmail.com>
2017-10-28 13:45:09 -0500

    Add extra test for file reader

--- tests/test_url.py
+++ tests/test_url.py
@@ -2,6 +2,8 @@ import os
 
 from html_cluster.utils.url import is_url, FileUrlsReader
 
+tests_datadir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
+
 
 def test_is_url():
     assert is_url('http://example.com')
@@ -9,9 +11,11 @@ def test_is_url():
     assert not is_url('text')
 
 
-tests_datadir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
-
-
-def test_file_reader():
+def test_file_reader_with_empty_lines():
     reader = FileUrlsReader(tests_datadir + '/urls-empty-lines.txt')
     assert 5 == len(list(reader.read()))
+
+
+def test_file_reader_with_empty_spaces():
+    reader = FileUrlsReader(tests_datadir + '/urls-empty-spaces.txt')
+    assert 4 == len(list(reader.read()))
