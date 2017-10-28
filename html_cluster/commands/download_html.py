11ecb7947f127ab0d16877da76a3d12b175cbb10
Edgar Marca <matiskay@gmail.com>
2017-10-28 13:53:42 -0500

    Integrate FileUrlsReader with download_html command

--- html_cluster/commands/download_html.py
+++ html_cluster/commands/download_html.py
@@ -7,7 +7,9 @@ import requests
 from html_cluster.settings import (
     HTML_CLUSTER_DATA_DIRECTORY, SPLASH_URL, USER_AGENT, SPLASH_TIMEOUT
 )
-from html_cluster.utils.common import file_name, is_html_page_from_string
+from html_cluster.utils.common import file_name
+from html_cluster.utils.html import is_html_page_from_string
+from html_cluster.utils.url import FileUrlsReader
 
 HELP = '''
 '''
@@ -52,58 +54,49 @@ def download_html(urls_file, output_directory, is_splash_request_enable=False, s
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
-    with open(urls_file, 'r') as f:
-        lines = f.readlines()
-
-        total_of_lines = len(lines)
-
-        for index, line in enumerate(lines):
-            url = line.replace('\n', '').strip()
-            url = url.strip()
-            if not url:
-                continue
-
-            click.echo(
-                click.style(
-                    'Downloading {} ({}/{})'.format(url, index + 1, total_of_lines), blink=True, bold=True
-                )
+    urls = FileUrlsReader(urls_file).read()
+    for url in urls:
+        click.echo(
+            click.style(
+                'Downloading {}'.format(url), blink=True, bold=True
             )
-            try:
-                r = make_request(url, splash=is_splash_request_enable, splash_url=splash_url)
-                html = r.text
+        )
+        try:
+            r = make_request(url, splash=is_splash_request_enable, splash_url=splash_url)
+            html = r.text
 
-                if r.status_code == requests.codes.ok:
-                    html_file_name = file_name(url, html)
-                    click.echo(
-                        click.style(
-                            '  --> Saving {}'.format(html_file_name), fg='green'
-                        )
+            if r.status_code == requests.codes.ok:
+                html_file_name = file_name(url, html)
+                click.echo(
+                    click.style(
+                        '  --> Saving {}'.format(html_file_name), fg='green'
                     )
+                )
+
+                if is_splash_request_enable:
+                    json_response = json.loads(r.text)
+                    html = json_response['html']
+
+                if is_html_page_from_string(html):
+                    with open('{}/{}.html'.format(output_directory, html_file_name), 'w') as html_file:
+                        html_file.write(html)
 
                     if is_splash_request_enable:
-                        json_response = json.loads(r.text)
-                        html = json_response['html']
+                        with open('{}/{}.png'.format(output_directory, html_file_name), 'wb') as png_file:
+                            png_file.write(base64.b64decode(json_response['png']))
 
-                    if is_html_page_from_string(html):
-                        with open('{}/{}.html'.format(output_directory, html_file_name), 'w') as html_file:
-                            html_file.write(html)
-
-                        if is_splash_request_enable:
-                            with open('{}/{}.png'.format(output_directory, html_file_name), 'wb') as png_file:
-                                png_file.write(base64.b64decode(json_response['png']))
-
-                    else:
-                        click.echo(
-                            click.style('  --> The url {} is not an html file'.format(url), fg='red')
-                        )
                 else:
                     click.echo(
-                        click.style(
-                            '  --> The {} return a bad status code ({}).'.format(url, r.status_code), fg='red'
-                        )
+                        click.style('  --> The url {} is not an html file'.format(url), fg='red')
                     )
-            except Exception as e:
-                print('   --> Oh noes! {}'.format(e))
+            else:
+                click.echo(
+                    click.style(
+                        '  --> The {} return a bad status code ({}).'.format(url, r.status_code), fg='red'
+                    )
+                )
+        except Exception as e:
+            print('   --> Oh noes! {}'.format(e))
 
 
 @click.command(help=HELP, short_help=SHORT_HELP)
